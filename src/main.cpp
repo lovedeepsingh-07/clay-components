b8e2c8e6bc21a44d80a8a5a40543d0114bc908de
lovedeepsingh-07 <lovedeepsinghbrar07@gmail.com>
2025-09-18 13:47:06 +0530

    feat: add dropdown component

--- src/main.cpp
+++ src/main.cpp
@@ -322,51 +322,90 @@ int main() {
                     auto* default_dropdown_ctx =
                         layout_engine.get_element<LayoutEngine::component_context::Dropdown>("demo_dropdown_default"
                         );
-                    bool clicked =
-                        layout_components::button()
-                            .clay_man(clay)
-                            .engine(layout_engine)
-                            .id("demo_dropdown_default_DROPDOWN_BUTTON")
-                            .variant("primary")
-                            .text(
-                                (default_dropdown_ctx != nullptr
-                                 && default_dropdown_ctx->open)
-                                    ? "close"
-                                    : "open"
-                            )
-                            .build();
+                    bool clicked = layout_components::button()
+                                       .clay_man(clay)
+                                       .engine(layout_engine)
+                                       .id("demo_dropdown_default_BUTTON")
+                                       .variant("primary")
+                                       .text(
+                                           (default_dropdown_ctx != nullptr
+                                            && default_dropdown_ctx->open)
+                                               ? "close"
+                                               : "open"
+                                       )
+                                       .build();
                     layout_components::dropdown_container()
                         .clay_man(clay)
                         .engine(layout_engine)
                         .root_id("demo_dropdown_default")
                         .button_clicked(clicked)
                         .children([&]() {
-                            clay.element(Clay_ElementDeclaration{}, [&]() {
-                                clay.textElement(
-                                    "Dropdown Children 1",
-                                    Clay_TextElementConfig{
-                                        .textColor = app_utils::raylib_to_clay(
-                                            layout_engine.get_color("card-"
-                                                                    "foreground")
-                                        ),
-                                        .fontId = 0,
-                                        .fontSize = 20,
-                                    }
-                                );
-                            });
-                            clay.element(Clay_ElementDeclaration{}, [&]() {
-                                clay.textElement(
-                                    "Dropdown Children 2",
-                                    Clay_TextElementConfig{
-                                        .textColor = app_utils::raylib_to_clay(
-                                            layout_engine.get_color("card-"
-                                                                    "foreground")
-                                        ),
-                                        .fontId = 0,
-                                        .fontSize = 20,
-                                    }
-                                );
-                            });
+                            clay.textElement(
+                                "Dropdown Children 1",
+                                Clay_TextElementConfig{
+                                    .textColor = app_utils::raylib_to_clay(
+                                        layout_engine.get_color("card-"
+                                                                "foreground")
+                                    ),
+                                    .fontId = 0,
+                                    .fontSize = 20,
+                                }
+                            );
+                        })
+                        .build();
+                }
+                layout_components::close_dropdown_root(clay);
+                // custom dropdown
+                layout_components::dropdown_root()
+                    .clay_man(clay)
+                    .engine(layout_engine)
+                    .id("demo_dropdown_custom")
+                    .build();
+                {
+                    auto* default_dropdown_ctx =
+                        layout_engine.get_element<LayoutEngine::component_context::Dropdown>("demo_dropdown_custom"
+                        );
+                    bool clicked = layout_components::button()
+                                       .clay_man(clay)
+                                       .engine(layout_engine)
+                                       .id("demo_dropdown_custom_BUTTON")
+                                       .variant("primary")
+                                       .text(
+                                           (default_dropdown_ctx != nullptr
+                                            && default_dropdown_ctx->open)
+                                               ? "close"
+                                               : "open"
+                                       )
+                                       .build();
+                    layout_components::dropdown_container()
+                        .clay_man(clay)
+                        .engine(layout_engine)
+                        .style(Clay_ElementDeclaration{
+                            .layout = { .sizing = { .width = CLAY_SIZING_FIT(120),
+                                                    .height = CLAY_SIZING_FIT(37) },
+                                        .padding = { .left = 10, .right = 10, .top = 8, .bottom = 8 },
+                                        .childGap = 14,
+                                        .childAlignment = { .x = CLAY_ALIGN_X_LEFT, .y = CLAY_ALIGN_Y_CENTER },
+                                        .layoutDirection = CLAY_TOP_TO_BOTTOM },
+                            .backgroundColor = app_utils::raylib_to_clay(MAROON),
+                            .cornerRadius = CLAY_CORNER_RADIUS(0),
+                            .border = { .color = app_utils::raylib_to_clay(PURPLE),
+                                        .width = { 2, 2, 2, 2, 0 } },
+                        })
+                        .root_id("demo_dropdown_custom")
+                        .button_clicked(clicked)
+                        .children([&]() {
+                            clay.textElement(
+                                "Dropdown Children 2",
+                                Clay_TextElementConfig{
+                                    .textColor = app_utils::raylib_to_clay(
+                                        layout_engine.get_color("card-"
+                                                                "foreground")
+                                    ),
+                                    .fontId = 0,
+                                    .fontSize = 20,
+                                }
+                            );
                         })
                         .build();
                 }
